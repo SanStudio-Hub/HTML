5159154b81d9ce60960cd7271f8ddbfb9f5f6b0e
SanStudio [ SANTHOSH A 2026 ] <crazycangstersanthosh@gmail.com>
2026-06-27 20:20:07 +0530

    Add files via upload

---
+++ SanCode/sw.js
@@ -0,0 +1,43 @@
+const CACHE_NAME = 'vscode-text-tool-v2';
+const ASSETS = [
+  './VsCode.html',
+  './manifest.json',
+  'https://cdn.tailwindcss.com',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css'
+];
+
+self.addEventListener('install', (e) => {
+  self.skipWaiting();
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS).catch(() => {});
+    })
+  );
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) return caches.delete(key);
+        })
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((cachedResponse) => {
+      if (cachedResponse) return cachedResponse;
+      return fetch(e.request).then((networkResponse) => {
+        return networkResponse;
+      }).catch(() => {
+        if (e.request.mode === 'navigate') {
+          return caches.match('./VsCode.html');
+        }
+      });
+    })
+  );
+});
